917c36a7e8c475d49201abbfb23a70f538dce389
Logan <lgy00001@mix.wvu.edu>
2023-05-25 22:47:13 -0400

    Updates to script.

--- script.py
+++ script.py
@@ -7,32 +7,32 @@ pyautogui.FAILSAFE = False
 # Change this to change the delay between each click in seconds
 delay_seconds = 0.5
 # Change this to change the delay between each click in seconds on the agent selection screen
-delay_seconds_agent = 0.3
+delay_seconds_agent = 0.1
 
 # Dictionary of agents and their pixel coordinates hard coded. Because the game updates this changes often.
 # If I wrote a function to find the coordinates of the agents, it would be fruitless as the game updates the coordinates of the agents.
 agent_dictionary = {
-    "jett": [0,0],
-    "raze": [0,0],
-    "breach": [0,0],
-    "omen": [0,0],
-    "brimstone": [0,0],
-    "phoenix": [0,0],
-    "sage": [0,0],
-    "sova": [0,0],
-    "viper": [0,0],
-    "cypher": [0,0],
-    "reyna": [0,0],
-    "killjoy": [0,0],
-    "skye": [0,0],
-    "yoru": [0,0],
-    "astra": [0,0],
-    "kayo": [0,0],
-    "neon": [0,0],
-    "harbor": [0,0],
-    "chamber": [0,0],
-    "fade": [0,0],
-    "gekko": [0,0],
+    "astra": [537,931],
+    "breach": [621,931],
+    "brimstone": [705,931],
+    "chamber": [789,931],
+    "cypher": [873,931],
+    "fade": [957,931],
+    "jett": [1041,931],
+    "kayo": [1125,931],
+    "killjoy": [1209,931],
+    "neon": [1293,931],
+    "omen": [1377,931],
+    "phoenix": [537,1010],
+    "raze": [621,1010],
+    "reyna": [705,1010],
+    "sage": [789,1010],
+    "skye": [873,1010],
+    "sova": [957,1010],
+    "viper": [1041,1010],
+    "yoru": [1125,1010],
+    "gekko": [1209,1010],
+    "harbor": [1293,1010],
     }
 # Change the name here for the agent you want to click
 agent_x, agent_y = agent_dictionary["jett"]
