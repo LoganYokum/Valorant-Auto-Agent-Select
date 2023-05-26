9b70b565387397a114c24e6767910a6adb1017d3
Logan <lgy00001@mix.wvu.edu>
2023-05-25 23:01:36 -0400

    Fixed requirement for windows systems.

--- requirement.sh
+++ requirement.sh
@@ -6,14 +6,18 @@ os=$(uname)
 if [ "$os" == "Linux" ]; then
     yes | apt install python3-pip
     yes | apt-get install python3-tk python3-dev
+    yes | pip install python-xlib
+    yes | pip install pyautogui
+    yes | pip install keyboard
 elif [ "$os" == "Windows" ]; then
-    yes | curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-    yes | python get-pip.py
+    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+    python get-pip.py
+    pip install python-xlib
+    pip install pyautogui
+    pip install keyboard
 else
     echo "Unsupported operating system: $os"
 fi
 
-yes | pip install python-xlib
-yes | pip install pyautogui
-yes | pip install keyboard
+
 
