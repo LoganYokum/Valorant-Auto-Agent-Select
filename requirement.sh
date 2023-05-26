6eeebb3ca377be60e8ee432487d648c42d063741
Logan <lgy00001@mix.wvu.edu>
2023-05-25 22:53:09 -0400

    Updates to readme for linux systems.

--- requirement.sh
+++ requirement.sh
@@ -5,6 +5,7 @@ os=$(uname)
 # Check if the operating system is Linux
 if [ "$os" == "Linux" ]; then
     yes | apt install python3-pip
+    yes | apt-get install python3-tk python3-dev
 elif [ "$os" == "Windows" ]; then
     yes | curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
     yes | python get-pip.py
